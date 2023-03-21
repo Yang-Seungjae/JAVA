db04aaa62917bdb2068f2a9307a2fda98dc7df9a
Yang Seungjae <ouuous@gmail.com>
2023-03-21 13:11:59 +0900

    no message

--- src/kr/ac/kopo/day11/HomeworkMain01.java
+++ src/kr/ac/kopo/day11/HomeworkMain01.java
@@ -1,10 +1,11 @@
-package kr.ac.kopo.day11.homework;
+package kr.ac.kopo.day11;
 
 public class HomeworkMain01 {
 
 	public static void main(String[] args) {
 		
 		ScannerUtil scan = new ScannerUtil();
+		
 		
 		char c = scan.nextChar("문자를 입력 : ");	
 		StringUtil util = new StringUtil();
@@ -21,6 +22,16 @@ public class HomeworkMain01 {
 		int maxNum = util.max(num1, num2);
 		System.out.println(num1 + ", " + num2 + "중 큰수 : " + maxNum);
 		
+		
+		
+		
+		
+		String str = scan.nextString("문자열을 입력 : ");
+		String revStr = util.reverseString(str);
+		System.out.println("str : " + str );
+		System.out.println("revStr : " + revStr );
+		String upperStr = util.toUpperString(str);
+		System.out.println("대문자변환 : " + upperStr);
 	}
 }
 
--- src/kr/ac/kopo/day11/StringUtil.java
+++ src/kr/ac/kopo/day11/StringUtil.java
@@ -1,4 +1,4 @@
-package kr.ac.kopo.day11.homework;
+package kr.ac.kopo.day11;
 
 public class StringUtil {
 
